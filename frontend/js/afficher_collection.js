f595ec8865386473f155e17ef53c7ebefa09f09c
NunoDSF <64093185+NunoDSF@users.noreply.github.com>
2020-08-14 00:18:21 +0200

    Add files via upload

---
+++ frontend/js/afficher_collection.js
@@ -0,0 +1,14 @@
+function affichColle(id) { 
+    let xhr =new XMLHttpRequest(); //nouvelle requête
+    xhr.open("get","http://localhost/collec", true);  //demande la liste des collections
+    xhr.onload = function(){
+        let tabl= JSON.parse(xhr.response).slice(); 
+        let colle = "";
+        for(let i in tabl){  //boucle sur le tableau d'objet
+            colle += "<tr><td>"+tabl[i].nom+"</td><td>"+tabl[i].description+"</td><td>"+tabl[i].nomCateg+"</td><td><button onclick='voirPlus("+tabl[i].id_colle+")'>VOIR PLUS</button></td></tr>";  //tableau avec informations des collections
+        }
+        document.getElementById(id).innerHTML += colle  //ajout dans le html des options
+    };
+    xhr.send();  
+
+}
---
+++ frontend/js/ajout_collection.js
@@ -0,0 +1,11 @@
+function ajout() { 
+    let formu = document.getElementById('ajout');   //récup du formulaire
+    let selectElmt = document.getElementById("categ");  //récup des catégories
+    let valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].id;  //valeur selectionné dans categories
+    let xhr = new XMLHttpRequest();
+    xhr.open('get','http://localhost/ajoutCollection?nom='+formu.nom.value+'&description='+formu.description.value+'&id_categorie='+valeurselectionnee,true); 
+    xhr.send(); //envoie à la db 
+    formu.nom.value=''; //mets des vides dans le formulaire
+    formu.description.value='';
+    return false
+}
